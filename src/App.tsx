1a3ac03f2bcebfab5e338b7e95814be795b55d23
SirCodr <jotadeveloper00@gmail.com>
2024-07-04 14:00:43 -0500

    Add expense create route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import ExpensesUploadPage from './pages/expenses/upload'
 import ExpensesPage from './pages/expenses'
+import ExpensesCreatePage from './pages/expenses/create'
 
 function App() {
   return (
     <div className='app'>
       <Routes>
         <Route path='expenses' element={<ExpensesPage />} />
+        <Route path='expenses/create' element={<ExpensesCreatePage />} />
         <Route path='expenses/upload' element={<ExpensesUploadPage />} />
       </Routes>
     </div>
---
+++ src/pages/expenses/create.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/layout"
+import ExpensesAddForm from "../../components/expenses/add-form"
+
+function ExpensesCreatePage() {
+  return (
+    <Layout>
+      <ExpensesAddForm />
+    </Layout>
+  )
+}
+
+export default ExpensesCreatePage
