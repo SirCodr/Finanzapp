9a806bedb83635f7f9ec91b8524e9be24b622b5b
SirCodr <jotadeveloper00@gmail.com>
2024-07-08 10:41:38 -0500

    Refactor header

--- src/components/expenses/header.tsx
+++ src/components/expenses/header.tsx
@@ -1,19 +1,4 @@
-import { Button } from 'primereact/button'
-import { Dialog } from 'primereact/dialog'
-import { useState } from 'react'
-import ExpensesAddForm from './add-form'
-
 const ExpensesHeader = () => {
-  const [isModalVisible, setModal] = useState<boolean>(false)
-
-  function showModal() {
-    setModal(true)
-  }
-
-  function hideModal() {
-    setModal(false)
-  }
-
   return (
     <nav className='bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700'>
       <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
@@ -58,16 +43,15 @@ const ExpensesHeader = () => {
           <ul className='flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <li>
                 <a href='expenses/upload'
-                className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent'
+                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 aria-current='page'>
                     Upload
                 </a>
             </li>
             <li>
-              <Button label="Create" onClick={showModal} className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' />
-              <Dialog header='Add Expense' visible={isModalVisible} onHide={hideModal} draggable={false}>
-                <ExpensesAddForm onSuccess={hideModal} />
-              </Dialog>
+              <a href='expenses/create' className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'>
+                Create
+              </a>
             </li>
           </ul>
         </div>
