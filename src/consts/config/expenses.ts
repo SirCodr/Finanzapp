86f3930c0ad2c9c32e5d30d7806be6544b89bb82
SirCodr <jotadeveloper00@gmail.com>
2024-07-30 16:51:00 -0500

    Add sortable capability to expenses table

--- src/consts/config/expenses.ts
+++ src/consts/config/expenses.ts
@@ -3,21 +3,26 @@ import { formatCurrency } from "../../utils/currency";
 
 export const EXPENSES_TABLE_COLUMNS: ColumnProps[] = [
   {
-    field: 'date'
+    field: 'date',
+    sortable: true
   },
   {
-    field: 'category'
+    field: 'category',
+    sortable: true
   },
   {
     field: 'subCategory',
-    header: 'Sub category'
+    header: 'Sub category',
+    sortable: true
   },
   {
     field: 'paymentMethod',
-    header: 'Payment Method'
+    header: 'Payment Method',
+    sortable: true
   },
   {
     field: 'price',
-    body: ({ price }: { price: string | number }) => formatCurrency(price)
+    body: ({ price }: { price: string | number }) => formatCurrency(price),
+    sortable: true
   }
 ]
