94b398bd95d2444d3adc9aa997dd09480ab82652
SirCodr <jotadeveloper00@gmail.com>
2024-07-30 17:12:35 -0500

    Add filter capability to expenses table

--- src/components/expenses/list-view.tsx
+++ src/components/expenses/list-view.tsx
@@ -3,6 +3,11 @@ import { Column } from 'primereact/column'
 import { EXPENSES_TABLE_COLUMNS } from '../../consts/config/expenses';
 import { generateRandomKey } from '../../utils';
 import { LocalExpense } from '../../types/expenses';
+import { useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+import { Button } from 'primereact/button';
 
 interface Props {
   items: LocalExpense[]
@@ -10,10 +15,30 @@ interface Props {
 }
 
 function ExpensesListView(props: Props) {
+  const [filter, setFilter] = useState('')
+
+  const onGlobalFilterChange = (e) => {
+        const value = e.target.value;
+        setFilter(value);
+    }
+
+  const renderHeader = () => {
+        return (
+            <div className="flex justify-content-between">
+                <Button type="button" icon="pi pi-filter-slash" label="Clear" outlined onClick={() => setFilter('')} />
+                <IconField iconPosition="left">
+                    <InputIcon className="pi pi-search" />
+                    <InputText value={filter} onChange={onGlobalFilterChange} placeholder="Filtrar datos" />
+                </IconField>
+            </div>
+        );
+    };
+
+    const header = renderHeader();
 
   return (
     <div className='card'>
-      <DataTable value={props.items} paginator rows={15} tableStyle={{ minWidth: '50rem' }} scrollable loading={props.isLoading}>
+      <DataTable value={props.items} paginator filterDisplay='row' globalFilter={filter} rows={15} tableStyle={{ minWidth: '50rem' }} header={header} scrollable loading={props.isLoading}>
         {
           EXPENSES_TABLE_COLUMNS.map((column, index) => {
             const { field, header } = column
