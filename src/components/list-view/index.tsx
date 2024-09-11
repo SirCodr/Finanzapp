d1838e99b32ef1ea78ea9aabb22be3678f376f08
SirCodr <jotadeveloper00@gmail.com>
2024-09-11 16:03:16 -0500

    Format code and add removablesort property for list view component

--- src/components/list-view/index.tsx
+++ src/components/list-view/index.tsx
@@ -1,11 +1,11 @@
 import { DataTable } from 'primereact/datatable'
 import { Column, ColumnProps } from 'primereact/column'
-import { generateRandomKey } from '../../utils';
-import { LocalExpense } from '../../types/expenses';
-import { ChangeEvent, useState } from 'react';
-import { InputText } from 'primereact/inputtext';
-import { InputIcon } from 'primereact/inputicon';
-import { Button } from 'primereact/button';
+import { generateRandomKey } from '../../utils'
+import { LocalExpense } from '../../types/expenses'
+import { ChangeEvent, useState } from 'react'
+import { InputText } from 'primereact/inputtext'
+import { InputIcon } from 'primereact/inputicon'
+import { Button } from 'primereact/button'
 
 interface Props {
   columns: ColumnProps[]
@@ -17,37 +17,63 @@ function ListView(props: Props) {
   const [filter, setFilter] = useState('')
 
   const onGlobalFilterChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setFilter(value);
-    }
+    const value = e.target.value
+    setFilter(value)
+  }
 
   const renderHeader = () => {
-        return (
-            <div className="w-full flex justify-end">
-                <section className='flex gap-6'>
-                  <Button type="button" icon="pi pi-filter-slash" label="Limpiar" outlined onClick={() => setFilter('')} />
-                  <div className='flex items-center gap-x-2'>
-                      <InputIcon className="pi pi-search" />
-                      <InputText value={filter} onChange={onGlobalFilterChange} placeholder="Filtrar datos" type='search' />
-                  </div>
-                </section>
-            </div>
-        );
-    };
+    return (
+      <div className='w-full flex justify-end'>
+        <section className='flex gap-6'>
+          <Button
+            type='button'
+            icon='pi pi-filter-slash'
+            label='Limpiar'
+            outlined
+            onClick={() => setFilter('')}
+          />
+          <div className='flex items-center gap-x-2'>
+            <InputIcon className='pi pi-search' />
+            <InputText
+              value={filter}
+              onChange={onGlobalFilterChange}
+              placeholder='Filtrar datos'
+              type='search'
+            />
+          </div>
+        </section>
+      </div>
+    )
+  }
 
-    const header = renderHeader();
+  const header = renderHeader()
 
   return (
     <div className='card'>
-      <DataTable value={props.items} paginator filterDisplay='row' globalFilter={filter} rows={15} tableStyle={{ minWidth: '50rem' }} header={header} scrollable loading={props.isLoading}>
-        {
-          props.columns.map((column, index) => {
-            const { field, header } = column
-            return (
-              <Column key={generateRandomKey(index)} field={field} header={field ?? header} {...column} headerClassName='capitalize'></Column>
-            )
-          })
-        }
+      <DataTable
+        value={props.items}
+        paginator
+        filterDisplay='row'
+        globalFilter={filter}
+        rows={15}
+        tableStyle={{ minWidth: '50rem' }}
+        header={header}
+        scrollable
+        removableSort
+        loading={props.isLoading}
+      >
+        {props.columns.map((column, index) => {
+          const { field, header } = column
+          return (
+            <Column
+              key={generateRandomKey(index)}
+              field={field}
+              header={field ?? header}
+              {...column}
+              headerClassName='capitalize'
+            ></Column>
+          )
+        })}
       </DataTable>
     </div>
   )
